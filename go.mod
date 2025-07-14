c1c5b7f5904b6f70cce20609eefa770825f6da91
Dell <Dell@example.com>
2025-07-14 14:21:23 +0530

    Phases 2-3: Product CRUD, tests, integration script & CI workflow

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/Heisenberg270/ecommerce-go
 
 go 1.20
+
+require (
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/lib/pq v1.10.9
+)
+
+require github.com/go-chi/chi/v5 v5.2.2
+
+require github.com/DATA-DOG/go-sqlmock v1.5.2
--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/Heisenberg270/ecommerce-go/handlers"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-	http.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprintln(w, "OK")
+	initDB()
+
+	r := chi.NewRouter()
+	// health check
+	r.Get("/healthz", func(w http.ResponseWriter, _ *http.Request) {
+		w.Write([]byte("OK"))
 	})
-	http.ListenAndServe(":8080", nil)
+
+	// product routes
+	ph := handlers.NewProductHandler(db)
+	r.Route("/products", func(r chi.Router) {
+		r.Post("/", ph.Create)
+		r.Get("/", ph.List)
+		r.Get("/{id}", ph.Get)
+		r.Put("/{id}", ph.Update)
+		r.Delete("/{id}", ph.Delete)
+	})
+
+	log.Println("Starting server on :8080")
+	http.ListenAndServe(":8080", r)
 }
